a62a09fc871af7846cd1b9f6f2adafe2b8a2513f
anupkankale <anupkankaleak47@gmail.com>
2024-10-15 11:51:27 +0530

    Hoisting in javaScript

--- hoisting.js
+++ hoisting.js
@@ -1,4 +1,33 @@
-console.log(a);
+/*
+  the process where declarations (variables and functions) are moved to the top of their containing scope
+  during the compile phase, before the code is executed.
 
-var a = 
+ Not define :-When you try to reference a variable that hasn’t been declared
+  anywhere in your code, you get a ReferenceError with the message "x is not defined".
 
+
+  Undefine :-  When a variable is declared but hasn't been assigned a value,
+   its value is undefined.
+
+*/
+
+function studyUndefined(){
+    var x
+ console.log(x)
+}
+
+// hello();
+// console.log(hello)
+// console.log(a);
+// var a
+// function hello(){
+
+//     console.log("Wecome to the Code With JavaScript")
+
+// }
+
+
+studyUndefined()
+console.log(y) //  ReferenceError: y is not defined
+
+var y=10;
---
+++ notes/hoisting.md
@@ -0,0 +1,9 @@
+  the process where declarations (variables and functions) are moved to the top of their containing scope
+  during the compile phase, before the code is executed.
+
+  <h6>  Not define :- <h6>When you try to reference a variable that hasn’t been declared
+   anywhere in your code, you get a ReferenceError with the  message "x is not defined".
+
+
+  <h6> Undefine :-  <h6> When a variable is declared but hasn't been assigned a value,
+   its value is undefined.
