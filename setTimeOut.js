989ff4efad57e399d9af4be7b00e43ed36921292
anupkankale <anupkankaleak47@gmail.com>
2024-11-04 15:50:58 +0530

    SetTimeOut

---
+++ setTimeOut.js
@@ -0,0 +1,10 @@
+console.log("Start");
+
+//Its Set'a Dealy Of 1000 ms
+//setTimeout is a built-in JavaScript function
+// that sets a timer to execute a function after a specified delay
+setTimeout(function cd() {
+  console.log("Callbacks");
+}, 1000);
+
+console.log("End");
